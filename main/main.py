a4936d3311b8e14f382e23da8c109900e160dc5e
OlgaKuratkina <olga.kuratkina@gmail.com>
2018-08-12 17:52:56 +0300

    endpoint for cards

--- main/main.py
+++ main/main.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from main.models import VocabularyCard
+from .database import init_db
 application = Flask(__name__)
+
+init_db()
 
 
 @application.route("/")
 def hello():
-    return render_template('index.html', notes=[1,2,3])
+    return render_template('index.html')
 
 
-@application.route("/example")
-def index():
-    return 'lalalal'
+@application.route("/cards", methods=['POST', 'GET'])
+def cards():
+    if request.method == 'POST':
+        origin_word = request.form.get('origin_word')
+        translation = request.form.get('translation')
+        part_speech = request.form.get('part_speech')
+        language = request.form.get('language')
+        if origin_word and translation and language:
+            VocabularyCard.create(origin_word=origin_word, translation_word=translation, origin_language=language,
+                                  part_of_speech=part_speech)
+    list_cards = get_cards()
+    return render_template('vocabulary.html', cards=list_cards)
+
+
+def get_cards():
+    list_cards = list(VocabularyCard.select().tuples())
+    return list_cards
