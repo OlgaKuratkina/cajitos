b2627c91664fb53e55d5d59c786764fcda3ad149
OlgaKuratkina <olga.kuratkina@gmail.com>
2018-08-27 14:11:36 +0300

    hide settings

--- main/settings/env.py
+++ main/settings/env.py
@@ -1,7 +1,9 @@
 # PUT RDS connections here
+import os
+
 DB_URI = "postgresql://cajitos.c9dujl3wwcyr.us-east-2.rds.amazonaws.com"
 DB_PORT = 5432
 DB_HOST = 'cajitos.c9dujl3wwcyr.us-east-2.rds.amazonaws.com'
-DB_USER = 'cajitosmaster'
+DB_USER = os.environ.get('db_user')
 DB_NAME = 'cajitos_db'
-DB_PASS = 'cajitoscajitos'
+DB_PASS = os.environ.get('db_pass')
