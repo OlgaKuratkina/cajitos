617276de9be79c3b9d8e80af47ff4e107a90bc99
OlgaKuratkina <olga.kuratkina@gmail.com>
2019-01-13 16:24:34 +0200

    forms for reset password

--- cajitos_site/models.py
+++ cajitos_site/models.py
@@ -1,5 +1,6 @@
 import peewee as pw
 import datetime as dt
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from cajitos_site import db, login_manager, application
 from flask_login import UserMixin
 
@@ -35,6 +36,19 @@ class User(TimestampModel, UserMixin):
 
     def __repr__(self):
         return f"User(username={self.username}, email={self.email})"
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(application.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(application.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.get_by_id(user_id)
 
     @property
     def is_active(self):
