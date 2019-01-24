100c5238300e446728b8386ccd4503a37e0774ce
OlgaKuratkina <olga.kuratkina@gmail.com>
2019-01-24 18:08:38 +0200

    trying to configure logs

--- cajitos_site/__init__.py
+++ cajitos_site/__init__.py
@@ -1,8 +1,12 @@
+import os
+
+import logging
 import peewee as pw
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 import cajitos_site.settings as settings
 
@@ -44,3 +48,30 @@ def create_app():
 
     return application
 
+
+application = create_app()
+if not application.debug:
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/cajitos.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        application.logger.addHandler(file_handler)
+        application.logger.setLevel(logging.INFO)
+        application.logger.info('Cajitos startup')
