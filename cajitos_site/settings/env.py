718d34ddbddf0fa4a3875a4a9371f34eb4d19da0
OlgaKuratkina <olga.kuratkina@gmail.com>
2019-01-28 22:13:28 +0200

    rework settings

--- cajitos_site/settings/env.py
+++ cajitos_site/settings/env.py
@@ -1,19 +1,31 @@
 # PUT RDS connections here
+import logging
 import os
+
+
+def env(var_name, default=None):
+    return os.environ.get(var_name, default)
+
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+
+SQLALCHEMY_DATABASE_URI = env('POSTGRES_URI', 'sqlite:///:memory:')
 
 PER_PAGE = 3
 
 DB_PORT = 5432
-DB_HOST = os.environ.get('db_host')
-DB_USER = os.environ.get('db_user')
-DB_NAME = os.environ.get('db_name')
-DB_PASS = os.environ.get('db_pass')
+DB_HOST = env('DB_HOST', 'localhost')
+DB_USER = env('DB_USER')
+DB_NAME = env('DB_NAME')
+DB_PASS = env('DB_PASS')
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 
-MAIL_USERNAME = os.environ.get('EMAIL_USER')
-MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+MAIL_USERNAME = env('EMAIL_USER')
+MAIL_PASSWORD = env('EMAIL_PASS')
+
+logging.warning('Env settings are loaded.')
