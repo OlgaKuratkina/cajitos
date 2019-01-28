23ef0de4e926292578892b47341265596ebeba8f
OlgaKuratkina <olga.kuratkina@gmail.com>
2019-01-28 22:14:25 +0200

    rework app factory

--- cajitos_site/__init__.py
+++ cajitos_site/__init__.py
@@ -7,8 +7,11 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from logging.handlers import SMTPHandler, RotatingFileHandler
-
 import cajitos_site.settings as settings
+from configure import configure_app
+
+logger = logging.getLogger(__name__)
+
 
 db = pw.PostgresqlDatabase(
     settings.DB_NAME,
@@ -22,18 +25,14 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 
-def create_app():
-    application = Flask(__name__)
+def create_app(application=None, default_settings='cajitos_site.settings'):
+    application = Flask(__name__, instance_relative_config=True)
 
-    application.config['APPS'] = ['cajitos_site']
-    application.config['SECRET_KEY'] = settings.SECRET_KEY
-    application.config['MAIL_SERVER'] = settings.MAIL_SERVER
-    application.config['MAIL_PORT'] = settings.MAIL_PORT
-    # application.config['MAIL_USE_TLS'] = settings.MAIL_USE_TLS
-    application.config['MAIL_USERNAME'] = settings.MAIL_USERNAME
-    application.config['MAIL_PASSWORD'] = settings.MAIL_PASSWORD
-    application.config['PER_PAGE'] = settings.PER_PAGE
-    application.config['OWNERS'] = settings.OWNERS
+    application.config.from_object(default_settings)
+
+    with application.app_context():
+
+        configure_app(application)
 
     bcrypt.init_app(application)
     login_manager.init_app(application)
@@ -48,33 +47,13 @@ def create_app():
     application.register_blueprint(misc)
     application.register_blueprint(errors)
 
+    # Register models
+    from . import models as models
+    application.models = models
+
+    logger.debug('App created')
+
     return application
 
 
 application = create_app()
-if not application.debug:
-    if application.config['MAIL_SERVER']:
-        auth = None
-        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
-            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
-        secure = None
-        # if application.config['MAIL_USE_TLS']:
-        #     secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
-            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
-            toaddrs=application.config['OWNERS'], subject='Cajitos Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        application.logger.addHandler(mail_handler)
-
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/cajitos.log', maxBytes=10240,
-                                           backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        application.logger.addHandler(file_handler)
-        application.logger.setLevel(logging.INFO)
-        application.logger.info('Cajitos startup')
---
+++ configure.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from logging.handlers import RotatingFileHandler, SMTPHandler
+
+
+def configure_app(application):
+    if not application.debug:
+        if application.config['MAIL_SERVER']:
+            auth = None
+            if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+                auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+            secure = None
+            # if application.config['MAIL_USE_TLS']:
+            #     secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+                fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+                toaddrs=application.config['OWNERS'], subject='Cajitos Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            application.logger.addHandler(mail_handler)
+
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/cajitos.log', maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            application.logger.addHandler(file_handler)
+            application.logger.setLevel(logging.INFO)
+            application.logger.info('Cajitos startup')
