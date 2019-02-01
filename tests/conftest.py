f1eafdb7500e31240c4ed3d1f1090d42df744304
OlgaKuratkina <olga.kuratkina@gmail.com>
2019-02-01 23:14:00 +0200

    fixtures for app and db init and cleanuo

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,10 @@
 import peewee as pw
 import pytest
 from mixer.backend.peewee import mixer
+from cajitos_site import db
 from cajitos_site import models as mod
 
 from cajitos_site.utils import get_models_from_module
-
-
-@pytest.fixture(scope='session')
-def db():
-    db = pw.SqliteDatabase('cajitos.db')
-    _init_db(db)
-    yield db
-    _shutdown_db(db)
 
 
 @pytest.fixture(scope='session')
@@ -19,17 +12,18 @@ def app():
     from cajitos_site import create_app
     app = create_app(None, 'cajitos_site.settings.test')
     app.app_context().push()
-    return app
+    _init_db(db)
+    yield app
+    _shutdown_db(db)
 
 
 def _init_db(db):
     tables = get_models_from_module(mod)
-    # db.drop_tables(tables)
     db.create_tables(tables)
-    user1 = mixer.blend(mod.User)
-    user2 = mixer.blend(mod.User)
-    mixer.cycle(5).blend(mod.Post, author=user1)
-    mixer.cycle(5).blend(mod.Post, author=user2)
+    user1 = mixer.blend(mod.User, email=mixer.RANDOM)
+    user2 = mixer.blend(mod.User, email=mixer.RANDOM)
+    mixer.cycle(5).blend(mod.Post, author=user1, title=mixer.RANDOM, content=mixer.RANDOM)
+    mixer.cycle(5).blend(mod.Post, author=user2, title=mixer.RANDOM, content=mixer.RANDOM)
     mod.Followers.create(following_user=user2, followed_user=user1)
 
 
@@ -41,4 +35,3 @@ def _shutdown_db(db):
 @pytest.fixture
 def user():
     return mixer.blend(mod.User, username='John', email='john@mail.com', password='jjjj')
-
