037f504a66c7f5941246ce81289266152dcf9474
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-05-08 20:53:15 +0300

    refactor

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from cajitos_site import create_app, db
 from cajitos_site.models import User, Post, Followers
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Followers': Followers}
