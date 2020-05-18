00be2445da1d1ba131f3e7adbcff0e743a2ccc58
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-05-18 21:38:20 +0300

    try redirect

--- app.py
+++ app.py
@@ -13,11 +13,3 @@ if __name__ == '__main__':
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Followers': Followers,
             'Ingerdient': Ingredient, 'Drink': Drink}
-
-
-# @application.before_request
-# def before_request():
-#     if request.url.startswith('http://'):
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
