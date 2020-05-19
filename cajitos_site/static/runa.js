106079552898341ada28b19e19ca03942d040cb3
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-05-19 12:52:20 +0300

    fix runa

--- cajitos_site/static/runa.js
+++ cajitos_site/static/runa.js
@@ -12,9 +12,16 @@ function runa() {
           prediction = sense[is_inverse];
         }
     }
-    document.getElementById("text_runa").value=prediction;
+    document.getElementById("text_runa").innerText=prediction;
+    document.getElementById("title_runa").innerText="Руны говорят:"
 }
 function begin(){
 document.getElementById("runaimg").src=runs[24];
-document.getElementById("text_runa").value="Тут будет Ваше предсказание";
+document.getElementById("text_runa").innerText="Тут будет Ваше предсказание";
+document.getElementById("title_runa").innerText="Подумайте о своей проблеме или текущей ситуации";
+}
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
 }
