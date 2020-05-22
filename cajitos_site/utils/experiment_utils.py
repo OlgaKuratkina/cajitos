e3ded31939107c940ae64a0e7f95c7efee46b857
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-05-22 18:29:36 +0300

    try2

--- cajitos_site/utils/experiment_utils.py
+++ cajitos_site/utils/experiment_utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 file_url = "https://cajitos-my-test.s3.amazonaws.com/check/requirements.txt"
@@ -12,6 +13,7 @@ def check_file_works():
             result.append(f.readline())
 
     if os.path.exists(local_config):
-        with open(local_config, "r") as f:
-            result.append(f.readline())
+        with open(local_config) as json_file:
+            data = json.load(json_file)
+            result.append(data)
     return result
